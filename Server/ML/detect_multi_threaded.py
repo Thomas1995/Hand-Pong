0d22a319fa1763a2c145c295424cc2ec6395cda4
Ionut Calofir <calofir.ionut@yahoo.com>
2018-05-09 19:37:51 +0300

    Update model

--- Server/ML/detect_multi_threaded.py
+++ Server/ML/detect_multi_threaded.py
@@ -8,12 +8,11 @@ import tensorflow as tf
 from multiprocessing import Queue, Pool
 #from ML.utils import detector_utils as detector_utils
 
-frame_processed = 0
-score_thresh = 0.2
+# score_thresh = 0.2
 
 # Create a worker thread that loads graph and
 # does detection on images in an input queue and puts it on an output queue
-def worker(input_q, output_q, score_q, cap_params, frame_processed):
+def worker(input_q, score_q):
     hand_cascade = cv2.CascadeClassifier('ML/hand.xml')
     #detection_graph, sess = detector_utils.load_inference_graph()
     #sess = tf.Session(graph=detection_graph)
@@ -28,54 +27,43 @@ def worker(input_q, output_q, score_q, cap_params, frame_processed):
 
             score = -1
             #if scores[0] > score_thresh:
-                # score = cap_params['im_height'] / (cap_params['im_height'] + ((bottom + top) / 2))
-            #    score = 1 - ((boxes[0][2] * cap_params['im_height'] + boxes[0][0] * cap_params['im_height']) / 2) / (cap_params['im_height'])
+                # score = 1 - ((boxes[0][2] * cap_params['im_height'] + boxes[0][0] * cap_params['im_height']) / 2) / (cap_params['im_height'])
 
-            best_match = 0
-            best_x = -1
+            best_match = -1
             best_y = -1
-            best_w = -1
-            best_h = -1
             for (x,y,w,h) in hands:
-                if w + h > best_match:
+                if w * h > best_match:
                   best_match = w + h
-                  best_x = x
                   best_y = y
-                  best_w = w
-                  best_h = h
 
-            if best_y != -1:
+            if best_match != -1:
               score = 1 - (best_y / frame.shape[1])
+              if score > 0.2 and score < 0.9:
+                score = (score - 0.2) / 0.7
+              elif score <= 0.2:
+                score = 0
+              else:
+                score = 1
 
-            output_q.put(frame)
             score_q.put(score)
-
-            frame_processed += 1
         else:
-            output_q.put(frame)
             score_q.put(-1)
-    sess.close()
+    # sess.close()
 
 class Model(object):
     def __init__(self,
                  num_hands=1,
-                 width=300,
-                 height=300,
                  num_workers=6,
                  queue_size=5):
         """
         Args:
             num_hands: Max number of hands to detect.
-            width: Width of the frames.
-            height: Height of the frames.
             num_workers: Number of workers.
             queue_size: Size of the queue.
         """
 
         self._args = {}
         self._args['num_hands'] = num_hands
-        self._args['width'] = width
-        self._args['height'] = height
         self._args['num_workers'] = num_workers
         self._args['queue_size'] = queue_size
 
@@ -86,34 +74,26 @@ class Model(object):
 
     def load_model(self):
         self.input_q = Queue(maxsize=self._args['queue_size'])
-        self.output_q = Queue(maxsize=self._args['queue_size'])
         self.score_q = Queue(maxsize=self._args['queue_size'])
-
-        cap_params = {}
-        frame_processed = 0
-        cap_params['im_width'], cap_params['im_height'] = self._args['width'], self._args['height']
-        cap_params['score_thresh'] = score_thresh
 
         # max number of hands we want to detect/track
         cap_params['num_hands_detect'] = self._args['num_hands']
 
-        print(cap_params, self._args)
+        print(self._args)
 
         # spin up workers to paralleize detection.
         self.pool = Pool(self._args['num_workers'], worker,
-                         (self.input_q, self.output_q, self.score_q, cap_params, frame_processed))
+                         (self.input_q, self.score_q))
 
     def inference_frame(self, frame):
-        # frame = video_capture.read()
         if self.index == 0:
             self.start_time = datetime.datetime.now()
-
-        # frame = cv2.flip(frame, 1)
-        self.index += 1
+        self.index = 1
 
         self.input_q.put(frame)
-        self.output_q.get()
         score = self.score_q.get()
+
+        print(self.input_q.qsize, self.score_q.qsize)
 
         elapsed_time = (datetime.datetime.now() -
                         self.start_time).total_seconds()
